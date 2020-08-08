c042f39ce853f37208e5f62aeea6ae2bcae3f907
masaki <hb.kmaia93@icloud.com>
2020-08-09 08:06:34 +0900

    route.rb, controllerのリダイレクトパス, 一覧表示画面のパスをresourceを用いた書き方に修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
-  get '/users', to: 'users#index'
-  get '/users/show'
-  get '/users/:id/edit', to: 'users#edit'
-  patch '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
