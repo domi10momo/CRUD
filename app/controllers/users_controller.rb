a6fa4afa402dfea3c7477ea0e65a921cf638d2a8
masaki <hb.kmaia93@icloud.com>
2020-08-09 00:02:48 +0900

    Userモデル作成、新規登録機能追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/users/new', to: 'users#new'
+  post '/users/create', to: 'users#create'
+  get '/users/index'
+  get '/users/show'
+  get '/users/edit'
+  get '/users/update'
+  get '/users/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
